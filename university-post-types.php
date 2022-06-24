f957cac6261294ba91aa046a26129b05d85f3ef8
itscsp <chethanspoojary1@gmail.com>
2022-06-24 07:07:44 +0530

    composes not competed

--- university-post-types.php
+++ university-post-types.php
@@ -1,6 +1,26 @@
 <?php
     function university_post_types() {
+        //Campus Post Type
+        register_post_type('campus', array(
+            'supports' => array('title', 'editor'),
+            'rewrite' => array('slug' => 'campuses'),
+            'has_archive' => true,
+            'public' => true,
+            'labels' => array(
+                'name' => 'Campuses',
+                'add_new_item' => 'Add New Campus',
+                'edit_item' => 'Edit Campus',
+                'all_items' => 'All Campuses',
+                'Singular_name' => 'Campus',
+
+            ),
+            'menu_icon' => 'dashicons-location-alt'
+        ));
+
+
+        //Event Post Type
         register_post_type('event', array(
+            'supports' => array('title', 'editor'),
             'rewrite' => array('slug' => 'events'),
             'has_archive' => true,
             'public' => true,
@@ -15,7 +35,9 @@
             'menu_icon' => 'dashicons-calendar'
         ));
 
+        //Program Post Type
         register_post_type('program', array(
+            'supports' => array('title', 'editor'),
             'rewrite' => array('slug' => 'programs'),
             'has_archive' => true,
             'public' => true,
@@ -29,8 +51,25 @@
             ),
             'menu_icon' => 'dashicons-awards'
         ));
+
+        //Professor Post Type
+        register_post_type('professor', array(
+            'supports' => array('title', 'editor', 'thumbnail'),
+            'public' => true,
+            'labels' => array(
+                'name' => 'Professors',
+                'add_new_item' => 'Add New Professor',
+                'edit_item' => 'Edit Professors',
+                'all_items' => 'All Professors',
+                'Singular_name' => 'Professor',
+
+            ),
+            'menu_icon' => 'dashicons-welcome-learn-more'
+        ));
+
     }
 
     add_action('init', 'university_post_types');
+
 
 
